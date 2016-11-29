2bad90592a380e02d1b1b2b9fb343bad673cfd15
Akash Yadav <akashyadav20111992@gmail.com>
2016-11-29 12:54:29 +0530

    slider fixed

--- views/js/main.js
+++ views/js/main.js
@@ -421,38 +421,22 @@ var resizePizzas = function(size) {
 
   changeSliderLabel(size);
 
-   // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
-  function determineDx (elem, size) {
-    var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
-    var oldSize = oldWidth / windowWidth;
-
-    // Changes the slider value to a percent width
-    function sizeSwitcher (size) {
-      switch(size) {
-        case "1":
-          return 0.25;
-        case "2":
-          return 0.3333;
-        case "3":
-          return 0.5;
-        default:
-          console.log("bug in sizeSwitcher");
-      }
-    }
-
-    var newSize = sizeSwitcher(size);
-    var dx = (newSize - oldSize) * windowWidth;
-
-    return dx;
-  }
-
-  // Iterates through pizza elements on the page and changes their widths
+  // this function simply takes the size to which all pizzas have to be converted
+  // and then selectively calculate size and then percentage size and put it there
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    switch(size) {
+      case "1":
+        newWidth = 25;
+      case "2":
+        newWidth = 33;
+      case "3":
+        newWidth = 50;
+      default:
+        console.log("bug in sizeSwitcher");
+    }
+    var pizzaSelectionAtOnce = document.querySelectorAll(".randomPizzaContainer");
+    for (var i = 0; i < pizzaSelectionAtOnce.length; i++) {
+      pizzaSelectionAtOnce[i].style.width = newWidth + "%"
     }
   }
 
@@ -500,10 +484,11 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
-
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  var long = items.length;
+  var phaseIn = document.body.scrollTop / 1250;
+  for (var i = 0; i < long; i++) {
+    var phase = Math.sin(phaseIn + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
